51a8517676015cb2af108d1fb587dbbdce9e8985
PyJacobito <sobel.jak@gmail.com>
2022-10-21 20:18:53 +0200

    4th_commit

--- expenses/templatetags/dict_val.py
+++ expenses/templatetags/dict_val.py
@@ -3,4 +3,8 @@ register = template.Library()
 
 @register.filter
 def dict_val(dict_var, key):
-    return dict_var[key]
+    try:
+        return dict_var[key]
+
+    except KeyError:
+        return 0
